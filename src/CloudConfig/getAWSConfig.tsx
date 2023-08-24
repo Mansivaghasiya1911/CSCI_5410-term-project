2357e4b828a80b3aecf87739f343496e6b942c3d
mansi <mansi.v@turabit.com>
2023-08-24 16:56:52 -0300

    Serverless term project

---
+++ src/CloudConfig/getAWSConfig.tsx
@@ -0,0 +1,11 @@
+export const AWSConfig = {
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: "*",
+    secretAccessKey: "*",
+    sessionToken: "*",
+  },
+};
+export const questionsDynamoTableName = "sdpQuestions";
+
+export const userTableName = "users";
